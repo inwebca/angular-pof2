158e5f773eaa0790928f92501159b10d9e05c9af
inwebca <blouinolivier@gmail.com>
2021-03-04 21:51:49 -0500

    4 files modified

--- src/app/survey-dialog-component/survey-dialog.component.ts
+++ src/app/survey-dialog-component/survey-dialog.component.ts
@@ -17,75 +17,14 @@ import {
   templateUrl: "./survey-dialog.component.html"
 })
 export class SurveyDialogComponent implements OnInit {
-  formGroup: FormGroup;
-  steps: Step[];
-
   constructor(
     public dialogRef: MatDialogRef<SurveyDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: IDriverSurvey,
-    private fb: FormBuilder
+    @Inject(MAT_DIALOG_DATA) public data: IDriverSurvey
   ) {}
 
-  get minMax() {
-    return this.formGroup.get("minMax") as FormGroup;
-  }
-
-  get multipleChoice() {
-    return this.formGroup.get("multipleChoice") as FormGroup;
-  }
-
-  getType(question): string {
-    if (this.isMinMax(question)) return "minMax";
-    if (this.isMultipleChoice(question)) return "multipleChoice";
-  }
-
-  ngOnInit() {
-    this.steps = this.data.questions.map(
-      question =>
-        ({
-          name: question.displayName,
-          kind: this.getType(question)
-        } as Step)
-    );
-
-    this.formGroup = this.fb.group({});
-
-    this.data.questions.forEach(question => {
-      if (this.isMinMax(question)) {
-        this.formGroup.addControl(
-          "minMax",
-          this.fb.group({
-            min: [question.choosedMin],
-            max: [question.choosedMax]
-          })
-        );
-      }
-      if (this.isMultipleChoice(question)) {
-        this.formGroup.addControl(
-          "multipleChoice",
-          this.fb.group({
-            values: [question.values],
-            choices: [question.choices]
-          })
-        );
-      }
-    });
-  }
+  ngOnInit() {}
 
   onClose(): void {
     this.dialogRef.close();
   }
-
-  isMinMax(object: any): object is IMinMaxQuestion {
-    return "choosedMin" in object;
-  }
-
-  isMultipleChoice(object: any): object is IMultipleChoiceQuestion {
-    return "choices" in object;
-  }
-}
-
-export class Step {
-  name: string;
-  kind: string;
 }
--- src/app/survey-stepper-component/survey-stepper.component.ts
+++ src/app/survey-stepper-component/survey-stepper.component.ts
@@ -1,11 +1,77 @@
 import { Component, Input, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import {
+  IDriverSurvey,
+  IMinMaxQuestion,
+  IMultipleChoiceQuestion
+} from "../services/survey.service";
 
 @Component({
   selector: "survey-stepper-component",
   templateUrl: "./survey-stepper.component.html"
 })
 export class SurveyStepperComponent implements OnInit {
-  constructor() {}
+  @Input() data: IDriverSurvey;
+  formGroup: FormGroup;
+  steps: Step[];
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit() {}
+  get minMax() {
+    return this.formGroup.get("minMax") as FormGroup;
+  }
+
+  get multipleChoice() {
+    return this.formGroup.get("multipleChoice") as FormGroup;
+  }
+
+  ngOnInit() {
+    this.steps = this.data.questions.map(
+      question =>
+        ({
+          name: question.displayName,
+          kind: this.getType(question)
+        } as Step)
+    );
+
+    this.formGroup = this.fb.group({});
+
+    this.data.questions.forEach(question => {
+      if (this.isMinMax(question)) {
+        this.formGroup.addControl(
+          "minMax",
+          this.fb.group({
+            min: [question.choosedMin],
+            max: [question.choosedMax]
+          })
+        );
+      }
+      if (this.isMultipleChoice(question)) {
+        this.formGroup.addControl(
+          "multipleChoice",
+          this.fb.group({
+            values: [question.values],
+            choices: [question.choices]
+          })
+        );
+      }
+    });
+  }
+
+  getType(question): string {
+    if (this.isMinMax(question)) return "minMax";
+    if (this.isMultipleChoice(question)) return "multipleChoice";
+  }
+
+  isMinMax(object: any): object is IMinMaxQuestion {
+    return "choosedMin" in object;
+  }
+
+  isMultipleChoice(object: any): object is IMultipleChoiceQuestion {
+    return "choices" in object;
+  }
+}
+
+export class Step {
+  name: string;
+  kind: string;
 }
