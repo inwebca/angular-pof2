382d7315fff4c3523cd39ba833455f77511116b0
inwebca <blouinolivier@gmail.com>
2021-03-05 08:49:47 -0500

    1 file modified

--- src/app/survey-stepper-component/survey-stepper.component.ts
+++ src/app/survey-stepper-component/survey-stepper.component.ts
@@ -1,10 +1,13 @@
 import { Component, Input, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup } from "@angular/forms";
 import {
+  IChoice,
   IDriverSurvey,
   IMinMaxQuestion,
   IMultipleChoiceQuestion,
-  IMultipleNestedChoiceQuestion
+  IMultipleNestedChoiceQuestion,
+  INestedChoice,
+  IQuestion
 } from "../services/survey.service";
 
 @Component({
@@ -30,9 +33,12 @@ export class SurveyStepperComponent implements OnInit {
       question =>
         ({
           name: question.displayName,
-          kind: this.getType(question)
+          kind: this.getType(question),
+          kind2: this.determineIfIsAnimalOrHuman(question)
         } as Step)
     );
+
+    console.log(this.steps);
 
     this.formGroup = this.fb.group({});
 
@@ -69,13 +75,35 @@ export class SurveyStepperComponent implements OnInit {
 
   isMultipleChoice(object: any): object is IMultipleChoiceQuestion {
     if ("values" in object) {
-      debugger;
+      return "values" in object;
     }
     return "choices" in object;
   }
 
   isMultipleNestedChoice(object: any): object is IMultipleNestedChoiceQuestion {
     return "choices" in object;
+  }
+
+  determineIfIsAnimalOrHuman(question: IQuestion) {
+    debugger;
+    if ((question as IMinMaxQuestion).choosedMin !== undefined) {
+      return "minMax";
+    }
+    if (
+      (question as IMultipleChoiceQuestion | IMultipleNestedChoiceQuestion)
+        .values !== undefined
+    ) {
+      let values = (question as
+        | IMultipleChoiceQuestion
+        | IMultipleNestedChoiceQuestion).values;
+      let choices = values as IChoice[] | INestedChoice[];
+      let nestedChoices = values as INestedChoice[];
+
+      if (nestedChoices[0].children !== undefined) {
+        return "multipleNestedChoice";
+      }
+      return "multipleChoice";
+    }
   }
 }
 
